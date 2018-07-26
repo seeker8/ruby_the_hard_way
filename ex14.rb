dce6d1d76d5502fde8a42938e31987149e8512d2
seeker8 <seeker8@users.noreply.github.com>
2018-07-25 19:14:12 -0500

    Fix interpolation

--- ex14.rb
+++ ex14.rb
@@ -1,7 +1,7 @@
 user_name = ARGV.first # gets the first argument
 prompt = '> '
 
-puts 'Hi #{user_name}.'
+puts "Hi #{user_name}."
 puts "I'd like to ask you a few questions."
 puts "Do you like me #{user_name}?"
 print prompt
