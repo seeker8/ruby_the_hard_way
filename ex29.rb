851a01a4a57ac98fd95a33e3c285d24645033339
seeker8 <seeker8@users.noreply.github.com>
2018-07-31 21:53:54 -0500

    Add ex29 What if

---
+++ ex29.rb
@@ -0,0 +1,29 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+    puts "too many cats! The world is doomed!"
+end
+
+if people > cats
+    puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+    puts "The world is drooled on!"
+end
+
+if people > dogs
+    puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+    puts "People are grater than or equal to dogs."
+end
+
+if people == dogs
+    puts "People are dogs."
+end
